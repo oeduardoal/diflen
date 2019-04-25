50618f11502535c6e6c0a24c0f54f2eced6fa895
Eduardo Almeida <oeduardoal@gmail.com>
2019-04-25 00:41:28 -0300

    datas dinamicas

--- index.php
+++ index.php
@@ -316,7 +316,7 @@
               <div class="card-deck mb-3 text-center">
                 <div class="card mb-4 shadow-sm">
                   <div class="card-header">
-                    <h4 class="my-0 font-weight-normal"><?php echo $lote['nome'] ?></h4>
+                    <h4 class="my-0 font-weight-normal"><?php echo $lote['name'] ?></h4>
                     <p><?php echo $lote['description'] ?></p>
                   </div>
                   <div class="card-body">
