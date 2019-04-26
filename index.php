9409973fbcd35f20b1c529ed03f955d80125b666
Eduardo Almeida <oeduardoal@gmail.com>
2019-04-25 22:02:20 -0300

    datas dinamicas

--- index.php
+++ index.php
@@ -79,8 +79,8 @@
     <div class="intro-content display-table">
       <div class="table-cell">
         <div class="container">
-          <h4 class="display-6 color-d space-top fontGlobal">Conferência Diflen</h4>
-          <h1 class="intro-title mb-4 fontGlobal">WE ARE GLOBAL</h1>
+          <h4 class="display-6 color-d space-top fontGlobal">DIFLEN GLOBAL 2019</h4>
+          <h1 class="intro-title mb-4 fontGlobal">ALL IN</h1>
           <p class="intro-subtitle space-bottom"><span class="text-slider-items">Discipulando,e Formando,Lideres,Em nações,Somos todos Diflen</span><strong class="text-slider"></strong></p>
           <h2 class="mb-4 fontGlobal light subtitleGlobal">15 e 16 de Novembro 2019</h2>
           <a class="nav-link js-scroll" href="#subscriber">
